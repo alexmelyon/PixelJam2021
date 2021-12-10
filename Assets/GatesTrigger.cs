6963948d2cdcc273df34b07dd870860cc0eb1618
Aleksandr M <alexmelyon@gmail.com>
2021-12-11 00:08:49 +0500

    Close gates

---
+++ Assets/GatesTrigger.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GatesTrigger : MonoBehaviour
+{
+    public GameObject[] possibleObjects;
+    
+    public UnityEvent onEnter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (possibleObjects.Contains(other.gameObject))
+        {
+            onEnter.Invoke();
+        }
+    }
+}
