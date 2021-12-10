0f00a53d8554a3d8b917dc60723e4fef7a64ad7b
Aleksandr M <alexmelyon@gmail.com>
2021-12-10 19:40:27 +0500

    finger normalized

--- Assets/MovePanel.cs
+++ Assets/MovePanel.cs
@@ -31,16 +31,33 @@ public class MovePanel : MonoBehaviour
             bool res = RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, Input.mousePosition, Camera.current, out point);
             if (res)
             {
-                Debug.Log("POINT " + point);
+                // Debug.Log("POINT " + point);
                 circle.transform.position = point + defPos;
             }
         }
         else if (Input.GetMouseButton(0))
         {
+            var rect = circle.rectTransform;
+            Vector2 point;
+            bool res = RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, Input.mousePosition, Camera.current, out point);
+            if (res)
+            {
+                finger.transform.position = new Vector3(point.x, point.y, 0) + circle.transform.position;
+            }
         }
         else
         {
             circle.transform.position = defPos;
+            finger.transform.position = circle.transform.position;
+        }
+
+        Vector2 direction = finger.transform.position - circle.transform.position;
+        var radius = circle.rectTransform.sizeDelta.x / 2;
+        Vector2 normalized = direction.normalized;
+        if (direction.magnitude > radius)
+        {
+            finger.transform.position = normalized * radius;
+            finger.transform.position += circle.transform.position;
         }
     }
 }
