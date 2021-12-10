84bf97e72e03f974a5693ab3cb1ec2f7a5203c50
Aleksandr M <alexmelyon@gmail.com>
2021-12-10 17:13:51 +0500

    CameraContainer

---
+++ Assets/CameraContainer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContainer : MonoBehaviour
+{
+    private PlayerController dog;
+
+    private void Awake()
+    {
+        dog = FindObjectOfType<PlayerController>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (dog.transform.position - transform.position) / 10;
+    }
+}
